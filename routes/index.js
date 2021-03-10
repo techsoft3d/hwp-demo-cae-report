1196c381ad218ac06eab7bc9fb22d2e4623d0c2d
Jingsong Tan <jingsong.tan@techsoft3d.com>
2021-03-09 22:08:22 -0600

    node-dev support

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+  // logger.statsd.increment('demos.cae_report');
+  res.render('cae-report', { title: 'CAE Analysis Report | HOOPS Web Viewer'});
 });
 
 module.exports = router;
